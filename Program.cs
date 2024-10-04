6ca12143481c6ce0b6801d656624d3ecc7a0e73f
Sellak Achraf <141083199+Ashrafsellak9@users.noreply.github.com>
2024-10-04 09:18:04 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCompteBancaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        class CompteBancaire
+        {
+            public string Titulaire
+            {
+                get;
+            }
+            public int NumeroCompte {  
+                get;
+            }
+            public decimal Solde
+            {
+                get;
+                protected set;
+            }
+            public List<string> Transactions
+            {
+                get;
+                private set;
+            }
+            
+            // Constructor
+            public CompteBancaire(string titulaire, int numeroCompte, decimal soldeInitial)
+            {
+                Titulaire = titulaire;
+                NumeroCompte = numeroCompte;
+                Solde = soldeInitial;
+                Transactions = new List<string>();
+            }
+
+            // Methods
+    }
+}
